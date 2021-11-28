ce01f2b0ff4e4c8f09b3f6462275b5474b69c8aa
Patekyy <93965327+Patekyy@users.noreply.github.com>
2021-11-28 07:43:33 +0300

    Update donate.js

--- plugins/donate.js
+++ plugins/donate.js
@@ -2,8 +2,8 @@ let handler = async m => m.reply(`
 
 
 ╭─「 Support link」
-│ • paytm,Donate [917736308760]
-│ • https://github.com/amal-dx/DXTROX-v2
+│ • paytm,Donate [254727475670]
+│ • https://github.com/Tracertech
  | • give a star if the project helped u
 ╰────
 `.trim()) // give a star if it helped u
